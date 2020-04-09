58258699e0ad62a156b80844a4ea8dc63dabeac4
joshuaasare <joshuaasare29@gmail.com>
2020-04-09 18:46:09 +0000

    changed logo

--- src/features/_shared/assets/index.js
+++ src/features/_shared/assets/index.js
@@ -1,4 +1,3 @@
-import afri from './images/afri.jpg';
 import schooldesk from './images/schooldesk_cover.jpg';
 import stcOperations from './images/stc_operations.jpg';
 import assetsManager from './images/assets_manager.jpg';
@@ -8,14 +7,14 @@ import dev from './animations/dev.json';
 import program from './animations/program.json';
 import resume from './animations/Joshua_resume.pdf';
 import logo from './images/Josh_logo.png';
+import exp1 from './images/exp1.png';
 
 export const images = {
-  afri,
   mmda,
   schooldesk,
   stcOperations,
   assetsManager,
-  logo,
+  logo: exp1,
 };
 
 export const files = {
